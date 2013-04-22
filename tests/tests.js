91fbb7df5cf6b2b28f2c952933e4b9661a5e63d3
Matthew Daly <matthew@astutech.com>
2013-04-22 10:23:05 +0100

    Added passing unit tests for alternate function

--- tests/tests.js
+++ tests/tests.js
@@ -9,4 +9,27 @@ $(function () {
         'alternate': true,
         'alternateclass': 'other'
     });
+
+    // Test alternate class applied correctly
+    test("Alternate class applied correctly", function () {
+
+        // Declare the variables used
+        var evenitems, item, odditems;
+
+        // Get all the even list items
+        evenitems = $('ul#mylist li:even');
+
+        // Get just the odd-numbered items (zero-indexed, so will be every second item
+        odditems = $('ul#mylist li:odd');
+
+        // Assert that each odd item has a class of 'other'
+        odditems.each(function () {
+            ok($(this).hasClass('other'), "Odd item has a class of 'other'");
+        });
+
+        // Assert that each even item does not have a class of 'other'
+        evenitems.each(function () {
+            ok(!$(this).hasClass('other'), "Event item does not have a class of 'other'");
+        });
+    });
 });
