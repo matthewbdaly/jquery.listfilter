ab7b467f6bc82bcd08f881ac27fc6ae7531b031c
Matthew Daly <matthew@astutech.com>
2013-04-22 09:57:10 +0100

    Now initialise listfilter for test

--- tests/tests.js
+++ tests/tests.js
@@ -1,4 +1,12 @@
 $(function () {
     'use strict';
 
+    // Apply the filter
+    var filter = $('input#filterinput'), clearfilter = $('input#clearfilter');
+    $('ul#mylist').listfilter({
+        'filter': filter,
+        'clearlink': clearfilter,
+        'alternate': true,
+        'alternateclass': 'other'
+    });
 });
